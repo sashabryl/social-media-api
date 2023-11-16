268e12e2ce25ff2675377da328b811ed4266108d
Olexander Bryl <edlrian814@gmail.com>
2023-11-16 21:15:41 +0200

    Add User model (AbstractUser)

--- user/models.py
+++ user/models.py
@@ -1,3 +1,22 @@
-from django.db import models
+import os
+import uuid
 
-# Create your models here.
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+from django.utils.text import slugify
+
+
+def user_picture_file_path(instance, filename) -> str:
+    _, extension = os.path.splitext(filename)
+    filename = f"{slugify(instance.picture)}-{uuid.uuid4()}{extension}"
+
+    return os.path.join("uploads/users/", filename)
+
+
+class User(AbstractUser):
+    bio = models.TextField()
+    picture = models.ImageField(
+        upload_to=user_picture_file_path,
+        null=True,
+        blank=True
+    )
